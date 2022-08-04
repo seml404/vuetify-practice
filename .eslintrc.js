5d96024014df06a34623034e945229fe6e3137ef
seml404 <4040591@gmail.com>
2022-08-04 15:39:32 +0300

    added selection for stepper

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   extends: ["plugin:vue/recommended"],
   rules: {
     "vue/multi-word-component-names": 0,
+    "vue/html-indent": 0,
+    "vue/html-self-closing": 0,
+    "vue/max-attributes-per-line": 0,
+    "vue/singleline-html-element-content-newline": 0,
   },
 };
